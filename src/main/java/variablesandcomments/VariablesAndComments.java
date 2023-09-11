277a806433bae7462242205fa13eb958bd12566b
mosgerila <calin.turcanu1@gmail.com>
2023-09-11 20:13:23 +0300

    Second commit - Variables and Comments

---
+++ src/main/java/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,33 @@
+package variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        //Declarare variabile
+
+        boolean isUsed;
+        boolean hasBattery;
+        boolean isAndroid;
+
+        char condition;
+
+        byte numberOfSimCards;
+        short productionYear;
+        int numberOfOwners;
+        long id;
+
+        float operatingSystemVersion;
+        double price;
+
+        // initierea unui telefon Iphone
+        isUsed = false;
+        hasBattery = true;
+        isAndroid = false;
+
+        condition = 'A';
+        productionYear = 2022;
+        numberOfSimCards = 2;
+        id = 787564565L;
+        operatingSystemVersion = 16.16F;
+        price = 24500.1234F;
+    }
+}
